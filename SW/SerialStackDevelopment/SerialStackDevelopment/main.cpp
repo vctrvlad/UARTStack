e79fd61b687ba04b3f132b376a13b8ce359d0be2
vctrvlad <vctrvlad@gmail.com>
2018-05-05 09:54:16 +0200

    Initial revision Signed-off-by: vctrvlad <vctrvlad@gmail.com>

---
+++ SW/SerialStackDevelopment/SerialStackDevelopment/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SerialStackDevelopment.cpp
+ *
+ * Created: 5/5/2018 7:41:13 AM
+ * Author : Victor
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
